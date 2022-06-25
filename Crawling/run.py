ec94a3998e904f5a9389098add174d9f46b4e353
ponopono0322 <ponopono0322@gmail.com>
2022-06-25 21:09:20 +0900

    Update run.py

--- Crawling/run.py
+++ Crawling/run.py
@@ -8,8 +8,9 @@ def main():
     # c_wanted.getdata()
 
     url = "https://job.incruit.com/jobdb_list/searchjob.asp?ct=1&ty=1&cd=150"
-    c_incruit = crawling_incruit.Incruit(url, True)
+    c_incruit = crawling_incruit.Incruit(url, False)
     c_incruit.getlink()
+    c_incruit.getdata()
 
 if __name__ == '__main__':
     main()
