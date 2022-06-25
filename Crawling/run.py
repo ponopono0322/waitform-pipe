6763f5235efddc7654d22108d8cd547378aa4f40
ponopono0322 <ponopono0322@gmail.com>
2022-06-25 20:45:45 +0900

    Update run.py

--- Crawling/run.py
+++ Crawling/run.py
@@ -1,10 +1,15 @@
 import crawling_wanted
+import crawling_incruit
 
 def main():
-    url = "https://www.wanted.co.kr/wdlist/518?country=kr&job_sort=company.response_rate_order&years=-1&locations=all"
-    c_wanted = crawling_wanted.Wanted(url, True)
-    c_wanted.getlink()
-    c_wanted.getdata()
+    # url = "https://www.wanted.co.kr/wdlist/518?country=kr&job_sort=company.response_rate_order&years=-1&locations=all"
+    # c_wanted = crawling_wanted.Wanted(url, True)
+    # c_wanted.getlink()
+    # c_wanted.getdata()
+
+    url = "https://job.incruit.com/jobdb_list/searchjob.asp?ct=1&ty=1&cd=150"
+    c_incruit = crawling_incruit.Incruit(url, True)
+    c_incruit.getlink()
 
 if __name__ == '__main__':
     main()
