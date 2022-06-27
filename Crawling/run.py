480d895b9a8868afb6e6d594e8f386b7e11ebea2
ponopono0322 <ponopono0322@gmail.com>
2022-06-27 11:12:37 +0900

    Update run.py
    function fix

--- Crawling/run.py
+++ Crawling/run.py
@@ -1,38 +1,41 @@
-import crawling_wanted
-import crawling_incruit
-import pandas as pd
+from crawling_wanted import Wanted
+from crawling_incruit import Incruit
+from CSVhandler import CSVhandler
 
 
 def main():
-    link_path = "./data/link.csv"
-    data_path = "./data/data.csv"
+    link_path = "link.csv"
+    data_path = "data.csv"
+    cvhd = CSVhandler()
+
     # wanted 사이트
     url = "https://www.wanted.co.kr/wdlist/518?country=kr&job_sort=company.response_rate_order&years=-1&locations=all"
-    c_wanted = crawling_wanted.Wanted(url, True)       # 새 객체 생성
-    wl_tmp = c_wanted.getlink()                        # 링크 데이터 크롤링
-    w_tmp = c_wanted.getdata()
-    c_wanted.close()
+    c_wanted = Wanted(url, True)        # 새 객체 생성
+    crawling(cvhd, c_wanted, link_path, data_path)
 
     # incruit 사이트
     url = "https://job.incruit.com/jobdb_list/searchjob.asp?ct=1&ty=1&cd=150"
-    c_incruit = crawling_incruit.Incruit(url, True)    # 새 객체 생성
-    il_tmp = c_incruit.getlink()                       # 랑크 데이터 크롤링
-    i_tmp = c_incruit.getdata()
-    c_incruit.close()
-
-    df = pd.concat([wl_tmp, il_tmp], ignore_index=True)
-    c_incruit.mergecsv(link_path, df)
-
-    df = pd.concat([w_tmp, i_tmp], ignore_index=True)
-    c_incruit.mergecsv(data_path, df)
-
-    c_wanted.quit()     # 크롬 드라이버 종료
-    c_incruit.quit()    # 크롬 드라이버 종료
+    c_incruit = Incruit(url, True)     # 새 객체 생성
+    crawling(cvhd, c_incruit, link_path, data_path)
 
     del c_wanted        # 소멸자 호출
     del c_incruit       # 소멸자 호출
 
     return 0
+
+
+def crawling(cvhd, c_class, link_path, data_path):
+    cl_tmp = c_class.getlink()                         # 링크 데이터 크롤링
+    cl_new = cvhd.searchnewdata(link_path, cl_tmp, 'link') # 새 데이터 찾기
+    c_class.datacheck(cl_new, type='link')             # 새 데이터 적용
+    c_tmp = c_class.getdata()                          # 공고문 데이터
+    c_new = cvhd.searchnewdata(data_path, c_tmp, 'data')
+
+    cvhd.mergecsv(link_path, cl_new, 'link')
+    cvhd.mergecsv(data_path, c_new, 'data')
+
+    c_class.close()
+    c_class.quit()     # 크롬 드라이버 종료
 
 if __name__ == '__main__':
     main()
