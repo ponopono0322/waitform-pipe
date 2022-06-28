4e6367cc4256d8711654eaad1a2af86bb105469f
ponopono0322 <ponopono0322@gmail.com>
2022-06-28 20:57:37 +0900

    Upload model-clustering

---
+++ Model/BertClassification.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import torch
+from torch import nn
+from torch.optim import Adam
+
+from transformers import BertModel
+from transformers import BertTokenizer
+
+import numpy as np
+
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')    # bert base model 사용
+labels = {  # 인덱스 설정
+    'backend%20developer': 0,
+    'software': 1,
+    'system': 2,
+    'database': 3,
+    'network': 4,
+    'Frontend%20Developer': 5,
+    'Application': 6,
+    'Service': 7,
+    'Game%20Developer': 8,
+    'AI%20Engineer': 9
+}
+
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, df):
+        self.labels = [labels[label] for label in df['category']]   # 라벨로 인덱스 설정
+        self.texts = [tokenizer(text,       # 단어 토큰화
+                                padding='max_length', max_length=512, truncation=True,
+                                return_tensors="pt") for text in df['text']]
+
+    # 라벨 반환 함수
+    def classes(self):
+        return self.labels
+
+    # 라벨 길이 반환 함수
+    def __len__(self):
+        return len(self.labels)
+
+    # Fetch a batch of labels
+    def get_batch_labels(self, idx):
+        return np.array(self.labels[idx])
+
+    # Fetch a batch of inputs
+    def get_batch_texts(self, idx):
+        return self.texts[idx]
+
+    # 인덱스와 데이터 가져오는 함수
+    def __getitem__(self, idx):
+
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+
+        return batch_texts, batch_y
+
+
+class BertClassifier(nn.Module):
+
+    def __init__(self, dropout=0.5):
+
+        super(BertClassifier, self).__init__()  # nn.Module 상속
+
+        self.bert = BertModel.from_pretrained('bert-base-cased')    # bert-base 모델 사용
+        self.dropout = nn.Dropout(dropout)      # set dropout
+        self.linear = nn.Linear(768, 10)        # 분류할 클래스가 10개
+        self.relu = nn.ReLU()                   # relu 활성화함수
+
+    def forward(self, input_id, mask):
+
+        _, pooled_output = self.bert(input_ids=input_id, attention_mask=mask, return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
+
+
+class BertClassification:
+
+    def __init__(self, model_path):
+        self.model = BertClassifier()
+        self.checkpoint = torch.load(model_path, map_location=torch.device('cpu'))
+        self.optimizer = Adam(BertClassifier().parameters(), lr=1e-6)  # init optimizer
+
+    def loader(self):
+        self.model.load_state_dict(self.checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(self.checkpoint['optimizer_state_dict'])
+
+    def evaluate(self, sentence):
+        sample = [['database', sentence]]
+        data = pd.DataFrame(sample, columns=['category', 'text'])
+        test = Dataset(data)
+        test_dataloader = torch.utils.data.DataLoader(test, batch_size=2)
+        device = torch.device("cpu")    # 테스트시에는 cpu할 사용할 예정
+        self.model.cpu()                # cpu로 조정
+
+        with torch.no_grad():
+            for test_input, _ in test_dataloader:   # 입력 데이터만 필요
+                mask = test_input['attention_mask'].to(device)  # cpu
+                input_id = test_input['input_ids'].squeeze(1).to(device)    # cpu
+
+                output = self.model(input_id, mask)  # 모델 결과
+                m = nn.Softmax(dim=1)                # softmax로 숫자값 추출
+                # this output sample: [[idx_0, idx_1, idx_2, idx_3, idx_4,
+                #                       idx_5, idx_6, idx_7, idx_8, idx_9]]
+                return m(output).cpu().numpy()[0]    # 결과 출력
---
+++ Model/Cluster.py
@@ -0,0 +1,168 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import pandas as pd
+import numpy as np
+
+
+class Cluster:
+
+    def __init__(self, board_idx):
+        self.df = None              # 모든 사용자의 데이터를 저장할 데이터프레임
+        self.dblink = None          # firebase 연동 객체를 담는 변수
+        self.board_idx = board_idx  # 게시물 데이터
+        # firebase 내 컬럼 값
+        self.column = ['c0', 'c1', 'c2', 'c3', 'c4',
+                       'c5', 'c6', 'c7', 'c8', 'c9', 'id']
+
+    # DB 연결 함수
+    def connect(self, pj_name):
+        # 아직 연동한 프로젝트가 없다면
+        if not firebase_admin._apps:
+            # 서비스 키를 사용해서 연동
+            cred = credentials.Certificate("./path/to/serviceAccountKey.json")
+            firebase_admin.initialize_app(cred, {   # 프로젝트 초기화
+                'projectId': pj_name                # 프로젝트 명에 따라 적용
+            })
+        self.dblink = firestore.client()            # 파이어베이스 클라이언트 접속
+
+    # 게시물 데이터 반환 함수
+    def getboard(self):
+        import re
+        board = self.dblink.collection(u'BOARD').document(
+            u"%s" % self.board_idx)                 # db에서 가져올 데이터 설정
+        board_data = board.get()                    # 데이터 가져오기
+        if board_data.exists:                       # 데이터가 존재한다면
+            tmp = board_data.to_dict()              # 사전 타입으로 변경
+            # 게시물 데이터, 게시물 쓴 사람
+            return re.sub('(<([^>]+)>)', '', tmp['content']), tmp['member']['id']
+        else:
+            return None, None                       # 데이터가 없다면 None 반환
+
+    # 모든 사용자 데이터 반환 함수
+    def getmembers(self, class_arr):
+        # 글에 대한 특징값 배열을 데이터프레임으로 변환
+        usr_df = pd.DataFrame([class_arr], columns=self.column)
+        if self.dblink:     # db에 데이터가 있다면
+            # 사용자 정보 긁어오기
+            docs = self.dblink.collection(u'MEMBER').stream()
+            # 사용자 정보 데이터프레임 생성
+            self.df = pd.DataFrame(columns=self.column)
+
+            for i, doc in enumerate(docs):  # 사용자 수 만큼 반복
+                # 임시 데이터프레임에 저장
+                tmp = pd.DataFrame(doc.to_dict(), index=[i])
+                self.df = pd.concat([self.df, tmp])  # 합치기
+            # 특징값과 함께 데이터프레임 완성
+            self.df = pd.concat([self.df, usr_df])
+        else:               # db에 데이터가 없거나 문제가 생겼다면
+            return "no connection!"
+
+    # 가상 사용자 생성 함수
+    def creator(self, n=1000):
+        import torch
+        from torch import nn
+
+        c_arr = []          # 가상 사용자 데이터를 담을 배열
+        for _ in range(n):  # n회 반복
+            arr2 = []       # 각 사용자의 데이터를 담을 배열
+            for i in range(10):  # 컬럼 수가 10개 이므로 10회만
+                # 2의 i승 사이의 값으로 설정
+                arr3 = np.random.randint(2 ** i, 2 ** (i + 1))
+                arr2.append(arr3)   # 배열에 추가
+            arr2 = np.random.permutation(arr2)  # 배열 섞기
+            c_arr.append(arr2 / np.linalg.norm(arr2))   # 정규화 수행
+
+        m = nn.Softmax(dim=1)       # softmax 클래스 가져오기
+        fake_arr = m(torch.Tensor(c_arr)).numpy()   # softmax 적용
+        fake_arr = np.append(fake_arr, [[i] for i in range(1, n + 1)], axis=1)
+
+        for i, j in enumerate(fake_arr):    # 만들어진 가짜 배열을 파이어베이스에 적용
+            data = self.dblink.collection(u'MEMBER')    # 사용자 테이블에 적용
+            data.document(u"%d" % (i + 1)).set({u'%s' % a: b for a, b in zip(self.column, j)})
+
+    # csv파일로부터 파이어베이스 가짜 데이터 적용
+    def creatorbycsv(self, df):
+        for i, j in enumerate(df.to_dict('records')):   # 데이터프레임 사전 타입 전환
+            data = self.dblink.collection(u"MEMBER")    # 사용자 테이블 지정
+            data.document(u"%d" % (i + 1)).set(j)       # 데이터 저장
+
+    # 500개의 가짜 데이터 생성
+    def checker(self):
+        df = pd.read_csv("conv2.csv", encoding='utf-8')  # 파일 읽어오기
+        cov_df = pd.DataFrame(columns=['c0', 'c1', 'c2', 'c3', 'c4', 'c5',
+                                       'c6', 'c7', 'c8', 'c9', 'category'])
+        for i in df.category.unique():                    # 10개의 컬럼 값
+            df2 = df[df['category'] == i].sample(n=50)    # 각 컬럼당 무작위 50개를 뽑아서
+            cov_df = pd.concat([cov_df, df2])             # 새 데이터 프레임에 합치기
+        cov_df.reset_index(drop=True, inplace=True)       # 인덱스 초기화
+        cov_df['id'] = [i for i in range(1, 501)]         # id 값 설정
+        cov_df.drop(['category'], axis=1, inplace=True)   # 컬럼 정보 삭제
+        self.creatorbycsv(cov_df)                         # 데이터베이스에 저장
+
+    # FIXME: 반복문 때문에 일부 사용자 없다면 오류
+    def clear(self, n=1000):    # 모든 사용자 제거 함수
+        for i in range(n):
+            self.dblink.collection(u'MEMBER').document(u"%d" % (i + 1)).delete()
+
+    # 클러스터링 함수
+    def cluster(self, k=10, max_return=5):    # 클러스터링
+        from sklearn.cluster import KMeans
+
+        model = KMeans(n_clusters=k, random_state=10)   # KMeans 클래스 사용
+        arr = self.df.drop(['id'], axis=1).to_numpy()   # id 컬럼은 사용하지 않으므로 제거
+        model.fit(arr)                                  # 학습
+        self.df['cluster'] = model.fit_predict(arr)     # 예측된 결과 컬럼 추가
+
+        num = self.df.iloc[-1]['cluster']               # 클러스터링 된 인덱스 번호
+        cdata = np.array(self.df.iloc[-1][:10])         # 입력 들어온 값의 데이터
+        same_data = self.df[self.df['cluster'] == num]  # 같은 클러스터 데이터프레임
+
+        result = np.array([])                           # 추천 유저 리스트
+        for i in same_data.index:                       # 같은 클러스터 반복
+            s = np.array(same_data.loc[i])[:10]         # 각 row 넘파이 변환
+            dist = np.linalg.norm(cdata-s)              # 거리 공식
+            result = np.append(result, [dist])          # 거리 추가
+        max_return += 1     # 인덱스 슬라이딩 때문에 추가
+        # 가장 근접한 데이터 저장
+        members = np.argpartition(result, (1, max_return))[1:max_return]
+        return [int(i) for i in members]                # 인덱스 출력
+
+    # 가장 근접한 결과를 출력하는 함수
+    def sortby(self, m_result, n):
+        sort_arr = [self.column[i] for i in np.argpartition(
+            -m_result, (1, len(m_result)-1))]
+        return self.df.sort_values(
+            by=sort_arr, axis=0, ascending=False)['id'][:n].to_numpy()
+
+    # 데이터 갱신 함수
+    def update(self, member_idx, class_arr):
+        # 데이터베이스에서 해당 사용자의 정보를 저장
+        member = self.dblink.collection(u'MEMBER').document(u"%s" % member_idx)
+        member_data = member.get()  # 데이터 기져오기
+        if member_data.exists:      # 데이터가 존재하는 경우만
+            tmp = member_data.to_dict()     # 사전 타입으로 변경
+            legacy_data = np.array([tmp[c] for c in self.column])   # 기존데이터
+            update_data = (legacy_data + class_arr) / 2.0           # 갱신데이터
+            # 갱신 데이터 적용
+            member.update({u'%s' % i: j for i, j in zip(self.column, update_data)})
+
+        else:   # 사용자를 찾지 못한 경우
+            print("Failed: member data update")  # 갱신 오류임을 알림
+
+    # 실제 사용한 학습 데이터로 softmax값을 뽑는 함수
+    def csvdbmaker(self, model_path, data_path, csv_name="conv2.csv"):
+        from BertClassification import BertClassification
+        b_class = BertClassification(model_path)
+        b_class.loader()    # 테스트하는 것과 같게
+        # 실제 사용한 데이터셋의 데이터를 불러와서 중복값, 결측값 제거
+        df = pd.read_csv(data_path).dropna().drop_duplicates(ignore_index=True)
+        ans_arr = []        # softmax 결과값을 저장하기 위한 함수
+
+        for i, j in zip(df['category'], df['text']):
+            sample = b_class.evaluate([[i, j]])                 # 모델로 결과 뽑기
+            ans_arr.append(np.append(sample, np.array([i])))    # 데이터 추가
+        # 데이터 프레임으로 전환
+        df2 = pd.DataFrame(ans_arr, columns=['c0', 'c1', 'c2', 'c3', 'c4',
+                                             'c5', 'c6', 'c7', 'c8', 'c9', 'category'])
+        df2.to_csv(csv_name, index=False, encoding='utf-8')     # 데이터 저장
